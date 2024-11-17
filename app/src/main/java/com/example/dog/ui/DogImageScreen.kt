86ce132eaa98bb54b20570267794782c7789e374
MEaston1 <mattheweaston21@gmail.com>
2024-11-17 22:49:50 +0000

    Responsive orientation
    Responsive layout orientation for the main screen.

--- app/src/main/java/com/example/dog/ui/DogImageScreen.kt
+++ app/src/main/java/com/example/dog/ui/DogImageScreen.kt
@@ -1,6 +1,8 @@
 package com.example.dog.ui
 
-import android.util.Log
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
@@ -23,41 +25,80 @@ import kotlinx.coroutines.launch
 // it within my composable functions as well, hitViewModel cannot be imported through an annotation on composable functions.
 @Composable
 fun DogImageScreen(navController: NavHostController, viewModel: DogViewModel = hiltViewModel()) {
+    val configuration = LocalConfiguration.current
+    val orientation = configuration.orientation
     val dogImage = viewModel.dogImage.collectAsState().value
     val coroutineScope = rememberCoroutineScope()
     LaunchedEffect (viewModel) {
         viewModel.fetchRandomDogImage()
     }
-    Log.d("DogImageScreen", "The dogImage is: $dogImage")
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        dogImage?.let {
-            Image(
-                painter = rememberAsyncImagePainter(it.url),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(300.dp)
-                    .testTag("dogImage"),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = { navController.navigate("breed/${it.breeds?.firstOrNull()?.id}") }) {
-                Text("Breed Details")
+    if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ){
+            dogImage?.let {
+                Image(
+                    painter = rememberAsyncImagePainter(it.url),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(300.dp)
+                        .testTag("dogImage"),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Column (
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Button(onClick = { navController.navigate("breed/${it.breeds?.firstOrNull()?.id}") }) {
+                        Text("Breed Details")
+                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+                                viewModel.fetchRandomDogImage()
+                            }
+                        },
+                        modifier = Modifier.testTag("fetchDogButton")
+                    ) {
+                        Text("Fetch Another Dog Image")
+                    }
+                }
             }
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(
-            onClick = {
-                coroutineScope.launch {
-                    viewModel.fetchRandomDogImage()
-                }
-            },
-            modifier = Modifier.testTag("fetchDogButton")
+    } else {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Text("Fetch Another Dog Image")
+            dogImage?.let {
+                Image(
+                    painter = rememberAsyncImagePainter(it.url),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(300.dp)
+                        .testTag("dogImage"),
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Button(onClick = { navController.navigate("breed/${it.breeds?.firstOrNull()?.id}") }) {
+                    Text("Breed Details")
+                }
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        viewModel.fetchRandomDogImage()
+                    }
+                },
+                modifier = Modifier.testTag("fetchDogButton")
+            ) {
+                Text("Fetch Another Dog Image")
+            }
         }
     }
 }
