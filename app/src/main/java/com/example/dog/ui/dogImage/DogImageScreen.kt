236daa0a8db8d281cac4ef7bf80ed697775ced2b
MEaston1 <mattheweaston21@gmail.com>
2026-07-24 16:34:02 +0100

    Deduplication of DogImageScreen.kt for orientation

--- app/src/main/java/com/example/dog/ui/dogImage/DogImageScreen.kt
+++ app/src/main/java/com/example/dog/ui/dogImage/DogImageScreen.kt
@@ -1,7 +1,6 @@
 package com.example.dog.ui.dogImage
 
 import android.content.res.Configuration
-import android.util.Log
 import androidx.compose.foundation.layout.Row
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.foundation.Image
@@ -11,6 +10,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -26,7 +26,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
@@ -34,6 +36,7 @@ import coil.compose.rememberAsyncImagePainter
 import com.example.dog.ui.components.FavouriteFAB
 import kotlinx.coroutines.launch
 import com.example.dog.R
+import com.example.dog.ui.DogImageUiState
 import com.example.dog.ui.DogViewModel
 import com.example.dog.ui.components.AnimalTabs
 
@@ -41,14 +44,15 @@ import com.example.dog.ui.components.AnimalTabs
 fun DogImageScreen(navController: NavHostController, dogViewModel: DogViewModel = hiltViewModel(), onBreedViewed: (String?) -> Unit) {
     val configuration = LocalConfiguration.current
     val orientation = configuration.orientation
-    val dogImage = dogViewModel.dogImage.collectAsState().value
+    val uiState = dogViewModel.uiState.collectAsState().value
     val coroutineScope = rememberCoroutineScope()
     val hasInitialFetch = remember { mutableStateOf(false) }
     val selectedTabIndex = remember { mutableIntStateOf(0) }
-    LaunchedEffect(dogImage) {
-        dogImage?.breeds?.firstOrNull()?.id?.let { breedId->
+
+    val dog = (uiState as? DogImageUiState.Success)?.dog
+    LaunchedEffect(dog) {
+        dog?.breeds?.firstOrNull()?.id?.let { breedId ->
             onBreedViewed(breedId)
-            Log.d("dogImage", "Breed ID is: $breedId")
         }
     }
 
@@ -66,148 +70,147 @@ fun DogImageScreen(navController: NavHostController, dogViewModel: DogViewModel 
         modifier = Modifier.background(MaterialTheme.colorScheme.background)
     )
     { paddingValues ->
-        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            Column(modifier = Modifier.fillMaxSize()) {
-                AnimalTabs(
-                    selectedTabIndex = selectedTabIndex.value,
-                    onTabSelected = { index -> selectedTabIndex.value = index }
-                )
-                Row(
+        Column(modifier = Modifier.fillMaxSize()) {
+            AnimalTabs(
+                selectedTabIndex = selectedTabIndex.intValue,
+                onTabSelected = { index -> selectedTabIndex.intValue = index }
+            )
+            when (uiState) {
+                DogImageUiState.Loading -> Box(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(paddingValues),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically,
+                    contentAlignment = Alignment.Center
                 ) {
-                    dogImage?.let {
-                        Image(
-                            painter = rememberAsyncImagePainter(it.url),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(250.dp)
-                                .clip(MaterialTheme.shapes.medium)
-                                .testTag("dogImage"),
-                            contentScale = ContentScale.Crop
-                        )
-                        Spacer(modifier = Modifier.width(50.dp))
-                        Card(
-                            shape = MaterialTheme.shapes.medium,
-                            elevation = CardDefaults.cardElevation(
-                                defaultElevation = 5.dp
-                            )
-                        ) {
-                            Column(
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                verticalArrangement = Arrangement.Center,
-                                modifier = Modifier.padding(16.dp)
-                            ) {
-                                Button(
-                                    modifier = Modifier
-                                        .testTag("breedDetailButton")
-                                        .size(250.dp, 50.dp),
-                                    shape = MaterialTheme.shapes.small,
-                                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
-                                    onClick = {
-                                        navController.navigate("breed/${it.breeds?.firstOrNull()?.id}") }) {
-                                    Text(
-                                        text = stringResource(id = R.string.pet_details),
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        color = MaterialTheme.colorScheme.onPrimary
-                                    )
-                                }
-                                Spacer(modifier = Modifier.height(16.dp))
-                                Button(
-                                    shape = MaterialTheme.shapes.small,
-                                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
-                                    onClick = {
-                                        coroutineScope.launch {
-                                            dogViewModel.fetchRandomDogImage()
-                                        }
-                                    },
-                                    modifier = Modifier
-                                        .testTag("fetchDogButton")
-                                ) {
-                                    Text(
-                                        text = stringResource(id = R.string.fetch_pet),
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        color = MaterialTheme.colorScheme.onSecondary
-                                    )
-                                }
-                            }
-                        }
-                    }
+                    CircularProgressIndicator()
                 }
-            }
-        } else {
-            Column(modifier = Modifier.fillMaxSize()) {
-                AnimalTabs(
-                    selectedTabIndex = selectedTabIndex.value,
-                    onTabSelected = { index -> selectedTabIndex.value = index }
-                )
-                Column(
+
+                DogImageUiState.Error -> Column(
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(paddingValues),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-                    dogImage?.let {
-                        Image(
-                            painter = rememberAsyncImagePainter(it.url),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(300.dp)
-                                .testTag("dogImage")
-                                .clip(MaterialTheme.shapes.medium),
-                            contentScale = ContentScale.Crop
+                    Image(
+                        painter = painterResource(id = R.drawable.errordog),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(300.dp)
+                            .clip(MaterialTheme.shapes.medium),
+                        contentScale = ContentScale.Crop
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = stringResource(id = R.string.info_failed_to_load),
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        shape = MaterialTheme.shapes.small,
+                        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
+                        onClick = { dogViewModel.fetchRandomDogImage() },
+                        modifier = Modifier.testTag("fetchDogButton")
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.fetch_pet),
+                            style = MaterialTheme.typography.bodyMedium,
+                            color = MaterialTheme.colorScheme.onSecondary
                         )
-                        Spacer(modifier = Modifier.height(100.dp))
-                        Card(
-                            shape = MaterialTheme.shapes.medium,
-                            elevation = CardDefaults.cardElevation(
-                                defaultElevation = 5.dp
-                            )
+                    }
+                }
+
+                is DogImageUiState.Success -> {
+                    val successDog = uiState.dog
+                    val contentModifier = Modifier
+                        .fillMaxSize()
+                        .padding(paddingValues)
+                    val onDetails: () -> Unit =
+                        { navController.navigate("breed/${successDog.breeds.firstOrNull()?.id}") }
+                    val onFetch: () -> Unit =
+                        { coroutineScope.launch { dogViewModel.fetchRandomDogImage() } }
+
+                    if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                        Row(
+                            modifier = contentModifier,
+                            horizontalArrangement = Arrangement.Center,
+                            verticalAlignment = Alignment.CenterVertically,
                         ) {
-                            Column(
-                                horizontalAlignment = Alignment.CenterHorizontally,
-                                verticalArrangement = Arrangement.Center,
-                                modifier = Modifier.padding(16.dp)
-                            ) {
-                                Button(
-                                    modifier = Modifier
-                                        .testTag("breedDetailButton")
-                                        .size(250.dp, 50.dp),
-                                    shape = MaterialTheme.shapes.small,
-                                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
-                                    onClick = { navController.navigate("breed/${it.breeds?.firstOrNull()?.id}") }) {
-                                    Text(
-                                        text = stringResource(id = R.string.pet_details),
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        color = MaterialTheme.colorScheme.onPrimary
-                                    )
-                                }
-                                Spacer(modifier = Modifier.height(16.dp))
-                                Button(
-                                    shape = MaterialTheme.shapes.small,
-                                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
-                                    onClick = {
-                                        coroutineScope.launch {
-                                            dogViewModel.fetchRandomDogImage()
-                                        }
-                                    },
-                                    modifier = Modifier
-                                        .testTag("fetchDogButton")
-                                ) {
-                                    Text(
-                                        text = stringResource(id = R.string.fetch_pet),
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        color = MaterialTheme.colorScheme.onSecondary
-                                    )
-                                }
-                            }
+                            DogImageView(url = successDog.url, size = 250.dp)
+                            Spacer(modifier = Modifier.width(50.dp))
+                            DogActionCard(onDetails = onDetails, onFetch = onFetch)
+                        }
+                    } else {
+                        Column(
+                            modifier = contentModifier,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center
+                        ) {
+                            DogImageView(url = successDog.url, size = 300.dp)
+                            Spacer(modifier = Modifier.height(100.dp))
+                            DogActionCard(onDetails = onDetails, onFetch = onFetch)
                         }
                     }
                 }
+            }
+        }
+    }
+}
+
+@Composable
+private fun DogImageView(url: String, size: Dp) {
+    Image(
+        painter = rememberAsyncImagePainter(url),
+        contentDescription = null,
+        modifier = Modifier
+            .size(size)
+            .clip(MaterialTheme.shapes.medium)
+            .testTag("dogImage"),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+private fun DogActionCard(
+    onDetails: () -> Unit,
+    onFetch: () -> Unit,
+) {
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Button(
+                modifier = Modifier
+                    .testTag("breedDetailButton")
+                    .size(250.dp, 50.dp),
+                shape = MaterialTheme.shapes.small,
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+                onClick = onDetails
+            ) {
+                Text(
+                    text = stringResource(id = R.string.pet_details),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                shape = MaterialTheme.shapes.small,
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary),
+                onClick = onFetch,
+                modifier = Modifier.testTag("fetchDogButton")
+            ) {
+                Text(
+                    text = stringResource(id = R.string.fetch_pet),
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSecondary
+                )
             }
         }
     }
